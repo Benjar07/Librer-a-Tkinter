05a69c87cf8f25bfb4803d0ddfdac3291e07a768
Benjar07 <07benjar@gmail.com>
2026-06-01 09:39:18 -0300

    Solucion de pequeño error

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+"""
+Estudiantes 7mo 3ra
+Benjamin Rodriguez
+Alejo Gomez
+"""
+
+#Importa la librerias de UIManager y ControlData de los directorios ui y data
 from ui.UIManager import window, ConfigWindow, CreateList, CreateOptionButtons
 from data.ControlData import LoadDataFromCSV
 
+#Utiliza una funcion de UIManager para configurar la resolucion de la ventana
 ConfigWindow(800, 600)
-
+#Carga los datos de un CSV, en las variables headers y data
 headers, data = LoadDataFromCSV("data/students.csv")
-
+#Crea una lista con los datos de headers y data
 CreateList(headers, data)
-
+#Crea los botones para las diferentes opciones
 CreateOptionButtons()
-
+#Ejecuta la aplicacion
 window.mainloop()
